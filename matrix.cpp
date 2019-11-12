21fdeb70caadf98a238eb506aeb65edb87a9f16f
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-11-12 01:43:09 -0500

    Parte 3

--- matrix.cpp
+++ matrix.cpp
@@ -17,7 +17,21 @@ void Matrix<T>::set(unsigned, unsigned, T){
 }
 
 template <typename T>
-T Matrix<T>::operator()(unsigned, unsigned) const {
+T Matrix<T>::operator()(unsigned f, unsigned c) const {// agregue el F C nose mmm vere vere
+    int i;
+    rows = f;
+    columns = c;
+    root = new T * [rows];
+    for(i=0; i<rows; i++){
+        root[i] = new T[columns];
+
+    }
+    for (int j=0;j<rows;j++){
+        for(int k=0;k<columns;k++){
+            root[j][k]=0;
+        }
+    }
+}
 
 }
 
@@ -26,27 +40,66 @@ Matrix<T> Matrix<T>::operator*(T scalar) const{
 
 }
 template <typename T>
-Matrix<T> Matrix<T>::operator*(Matrix<T> other) const{
+Matrix<T> Matrix<T>::operator*(Matrix<T> other) const{//Tengo que revisarlo xD
+    int i, j,k;
+    if (this->columns == other.rows) {
+        Matrix res(this->rows,other.columns);
+        for (i = 0; i < this->rows; i++){
+            for (j = 0; j < other.columns; j++){
+                for( k = 0 ; k < other.rows; k++)
+                    res.elementos[i][j] += root[i][k]*other.elementos[k][j];
+            }
+        }
+        return res;
+    } else{
+        cout<<"La Columna de M1 no es igual a la fila de M2"<<endl;
+    }
 }
 template <typename T>
-Matrix<T> Matrix<T>::operator+(Matrix<T> other) const{
-
+Matrix<T> Matrix<T>::operator+(Matrix<T> other) const{//Posiblemente
+    int i, j;
+    if (this->filas == other.filas && this->columnas == other.columnas) {
+        Matrix res(other);
+        for (i = 0; i < other.filas; i++)
+            for (j = 0; j < other.columnas; j++)
+                res.elementos[i][j] += this->elementos[i][j];
+        return res;
+    } else {
+        Matrix res;
+        return res;
+    }
 }
 template <typename T>
-Matrix<T> Matrix<T>::operator-(Matrix<T> other) const{
-
+Matrix<T> Matrix<T>::operator-(Matrix<T> other) const{//Solo cambiaria el signo mmm
+    int i, j;
+    if (this->filas == other.filas && this->columnas == other.columnas) {
+        Matrix res(other);
+        for (i = 0; i < other.filas; i++)
+            for (j = 0; j < other.columnas; j++)
+                res.elementos[i][j] -= this->elementos[i][j];
+        return res;
+    } else {
+        Matrix res;
+        return res;
+    }
 }
 template <typename T>
 Matrix<T> Matrix<T>::transpose() const{
 
 }
 template <typename T>
-void Matrix<T>::Mostrar() const{
-    cout<<"Hola";
+void Matrix<T>::Mostrar() const{//Espero no morir aqui
+    int i, j;
+    for(i=0; i<rows; i++){
+        for(j=0; j<columns; j++){
+            cout<<root[i][j]<<"  ";
+        }
+        cout<<endl;
+    }
 }
 
 
 template <typename T>
-Matrix<T>::~Matrix() {
-
+Matrix<T>::~Matrix() {//Aun no estoy seguro que esto sea asi xD puedo poner kills... pero no se
+    delete [] root;
 }
