b0b889756dfbbfcb5bc5d7e86c7da1d9b3f2da0d
davidalejandrolazopampa <40555746+davidalejandrolazopampa@users.noreply.github.com>
2019-11-12 01:50:29 -0500

    Parte 4

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 //David Lazo
 int main() {
     //Tester::execute();
+    int f1,c1,f2,c2;
+    Matrix m1(f1,c1);
+    m1.operator()();
+    m1.Mostrar();
+    m1.Mostrar();
+    m1=m1*m1;
+
     return EXIT_SUCCESS;
 
 }
